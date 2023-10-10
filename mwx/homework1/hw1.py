1475ebc7555abd8ff0574af4e3dde5bdda6b4ad4
tcbwk461 <147480695+tcbwk461@users.noreply.github.com>
2023-10-10 23:17:19 +0800

    Add files via upload

---
+++ mwx/homework1/hw1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('C:/Users/hp/Desktop/download.jpg')
+
+for i in [1,2,3]:
+    for j in [1,2,3]:
+        img[20*j-20:20*j,30*i-30:30*i]=img[60:80,160:190]
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(img.shape)
